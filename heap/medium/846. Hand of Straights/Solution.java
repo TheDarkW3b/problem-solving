021a2caa601912929c08b1b7c4df746e1ef04684
Ramit Gangwar (NoiR) <54339318+TheDarkW3b@users.noreply.github.com>
2024-10-25 22:35:59 +0530

    Added better solution

--- heap/medium/846. Hand of Straights/Solution.java
+++ heap/medium/846. Hand of Straights/Solution.java
@@ -1,3 +1,37 @@
+// HEAP SOLUTION
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(map.keySet());
+
+        while (!heap.isEmpty()) {
+            int current = heap.poll();
+
+            if (map.getOrDefault(current, 0) == 0) continue;
+
+            for (int i = 0; i < groupSize; i++) {
+                int required = current + i;
+                if (map.getOrDefault(required, 0) == 0) return false;
+
+                map.put(required, map.get(required) - 1);
+
+                if (map.get(required) == 0) map.remove(required);
+            }
+
+            if(map.getOrDefault(current, 0) != 0) heap.add(current);
+        }
+
+        return true;
+    }
+}
+
+// TREMAP SOLUTION
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if (hand.length % groupSize != 0) return false;
