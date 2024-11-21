cc1873aa2f3eaa13391d4ba5a2ba653505c4f6fd
Ramit Gangwar (NoiR) <54339318+TheDarkW3b@users.noreply.github.com>
2024-11-21 23:02:49 +0530

    Update Solution.java

--- array/medium/2257. Count Unguarded Cells in the Grid/Solution.java
+++ array/medium/2257. Count Unguarded Cells in the Grid/Solution.java
@@ -51,3 +51,52 @@ class Solution {
         return count;
     }
 }
+
+// Direction Arrays
+class Solution {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        boolean[][] seenByGuard = new boolean[m][n];
+        boolean[][] isWallOrGuard = new boolean[m][n];
+
+        for (int[] wall : walls) {
+            isWallOrGuard[wall[0]][wall[1]] = true;
+            seenByGuard[wall[0]][wall[1]] = true;
+        }
+
+        for (int[] guard : guards) {
+            isWallOrGuard[guard[0]][guard[1]] = true;
+            seenByGuard[guard[0]][guard[1]] = true;
+        }
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (int[] guard : guards) {
+            int row = guard[0];
+            int col = guard[1];
+
+            for (int[] direction : directions) {
+                int r = row, c = col;
+
+                while (true) {
+                    r += direction[0];
+                    c += direction[1];
+
+                    if (r >= m || r < 0 || c >= n || c < 0 || isWallOrGuard[r][c]) break;
+                    seenByGuard[r][c] = true;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!seenByGuard[i][j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
