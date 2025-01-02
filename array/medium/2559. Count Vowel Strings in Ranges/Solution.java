9689b66d5a9a5a20b8d259de13fdc4b3423ed3bc
Ramit Gangwar (NoiR) <54339318+TheDarkW3b@users.noreply.github.com>
2025-01-03 01:42:03 +0530

    Update Solution.java

--- array/medium/2559. Count Vowel Strings in Ranges/Solution.java
+++ array/medium/2559. Count Vowel Strings in Ranges/Solution.java
@@ -1,3 +1,33 @@
+// Optimised - Prefix Sum
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        Set<Character> vowelSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        int n = words.length;
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            prefix[i + 1] = prefix[i] + (isVowelString(word, vowelSet) ? 1 : 0);
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            ans[i] = prefix[end + 1] - prefix[start];
+        }
+
+        return ans;
+    }
+
+    private boolean isVowelString(String word, Set<Character> vowelSet) {
+        return vowelSet.contains(word.charAt(0)) && vowelSet.contains(word.charAt(word.length() - 1));
+    }
+}
+
+// Brute-force (TLE)
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         Set<Integer> vowels = new HashSet<>();
