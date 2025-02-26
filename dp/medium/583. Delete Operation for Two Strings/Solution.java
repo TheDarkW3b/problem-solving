934af10cfead5fef2923773bae1e839edd801a7f
Ramit Gangwar (NoiR) <54339318+TheDarkW3b@users.noreply.github.com>
2025-02-27 01:01:09 +0530

    Create Solution.java

---
+++ dp/medium/583. Delete Operation for Two Strings/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestCommonSubsequence(String x, String y) {
+        int m = x.length();
+        int n = y.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public int minDistance(String word1, String word2) {
+        return word1.length() + word2.length() - 2 * longestCommonSubsequence(word1, word2);
+    }
+}
